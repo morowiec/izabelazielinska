9f6a62f2f2b58b2b05b57b2d074703caa6877ad6
Bartek Zabdyr <bartlomiej.zabdyr@ext.ec.europa.eu>
2017-10-26 12:14:17 +0200

    Add copyright block

---
+++ sites/all/modules/izazielinska_site/src/Plugin/Block/CopyrightBlock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\izazielinska_site\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a block with a simple text.
+ *
+ * @Block(
+ *   id = "copyright_block_example_block",
+ *   admin_label = @Translation("CopyrightBlock"),
+ * )
+ */
+class CopyrightBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => '<span>' . $this->t('Copyright izabelazielinska.pl 2009-:year', [':year' => date("Y")]) . '</span>'
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['copyright_block_settings'] = $form_state->getValue('copyright_block_settings');
+  }
+}
